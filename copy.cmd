d2bf9ca500e8ea68c071685e29d27c471933dba8
Ilya <test@gmail.com>
2026-05-08 13:55:11 +0300

    It's obviously new file

---
+++ copy.cmd
@@ -0,0 +1,17 @@
+@echo off
+echo "Copy files from this path to another one"
+
+if not exist H:\Database (
+	mkdir H:\Database
+) else (
+	echo "You already have this Data"
+)
+
+echo "Start copy"
+
+for /R %%i in (*) do (
+	echo %%i
+	copy %%i H:\Database
+)
+
+echo on
