3ccd896b6943551a456615216b11179d2598d476
MyWizer <white.wizer@gmail.com>
2026-05-08 15:11:15 +0300

    Hello

--- create_dir_tree.cmd
+++ create_dir_tree.cmd
@@ -2,17 +2,8 @@
 if not exist main (
 	echo "Create directory tree"
 	mkdir main
-	mkdir main\001
-	mkdir main\002
-	mkdir main\001\a
-	mkdir main\001\b
-	mkdir main\002\c
-	mkdir main\002\d
+	"Hi"
 ) else (
 	echo "Sorry, directory had already made"
 )
-echo "You did it, your first commit"
-echo "IT'S ABSOLUTELY NEW TEXT!"
-echo "IT'S ABSOLUTELY NEW TEX2T!"
-echo "IT'S ABSOLUTELY NEW TEXdfhkuasdhgkashgikausdh!"
 echo ON
