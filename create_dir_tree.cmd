e4d981c5b6377f610683335209acc263b8f31186
Ilya <test@gmail.com>
2026-05-08 13:25:02 +0300

    My first commit. You

---
+++ create_dir_tree.cmd
@@ -0,0 +1,14 @@
+@echo OFF
+if not exist main (
+	echo "Create directory tree"
+	mkdir main
+	mkdir main\001
+	mkdir main\002
+	mkdir main\001\a
+	mkdir main\001\b
+	mkdir main\002\c
+	mkdir main\002\d
+) else (
+	echo "Sorry, directory had already made"
+)
+echo ON
